da4e5df162d115bb76ada123b140aae6bb374ec2
banhtieu <thienkhiem88@gmail.com>
2015-01-31 11:11:38 +0700

    + Add component class for all Component

---
+++ Application/Component/Component.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: banht_000
+ * Date: 1/31/2015
+ * Time: 9:15 AM
+ */
+
+namespace Application\Component;
+
+/**
+ * Class Component Core Class for all polymer client
+ * @package Application\Component
+ */
+class Component {
+
+
+    /**
+     *  First time load this Component
+     */
+    public function onLoad(){
+
+    }
+
+}
